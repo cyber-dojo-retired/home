b07a35571f2d3ae941ee23ece202ab38203b4405
Jon Jagger <jon@jaggersoft.com>
2020-09-28 21:00:58 +0100

    Refactoring

--- source/server/code/helpers/app_helpers.rb
+++ source/server/code/helpers/app_helpers.rb
@@ -4,10 +4,7 @@ require_relative 'phonetic'
 module AppHelpers
 
   def phonetic(id)
-    letters = Phonetic.spelling(id)
-    lhs = letters[0..2].join('-')
-    rhs = letters[3..-1].join('-')
-    lhs + '<br/>' + rhs
+    Phonetic.spelling(id).join('<br/>');
   end
 
 end
